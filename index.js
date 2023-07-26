1a67914959d921b792e4ba78495b62d19d552431
Gregorio <Greg.del801@gmail.com>
2023-07-25 19:13:45 -0700

    updated algorithm

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 function hasTargetSum(array, target) {
-tF = false
-  // Write your algorithm here
-for(let i = 0; i < array.length; i++){
-  num1 = array[i]
-  for (let j = i + 1; j< array.length; j++){
-    num2 = array[j]
-    console.log(`${num1}+${num2}=${num1 + num2}    target? ${num1 + num2 === target}`)
-    if(num1 + num2 === target){
-      tF = true
-    }
+  const seenNumbers = {}
+  //create an object to keep track of numbers we've already seen
+  for(const number of array) {
+    //for the current num, identify a compliment thta adds to the target (comp = target - num)
+    const compliment = target - number
+    //check if any key on our object is the compliment
+    //if so return true
+    if(compliment in seenNumbers) return true
+    //othrewise add that number to the object
+    seenNumbers[number] = true 
   }
+  return false
+}
 
-}
- return tF
-}
+
+
+
+
+
+// function hasTargetSum(array, target) {
+// tF = false
+//   // Write your algorithm here
+// for(let i = 0; i < array.length; i++){
+//   //n steps
+//   num1 = array[i]
+//   for (let j = i + 1; j< array.length; j++){
+//     //n * n steps
+//     num2 = array[j]
+//     // console.log(`${num1}+${num2}=${num1 + num2}    target? ${num1 + num2 === target}`)
+//     if(num1 + num2 === target){
+//       tF = true
+//     }
+//   }
+
+// }
+//  return tF
+// }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here  
  
   add first number to each number 
+  if first number added to any other number does not equal target
+  repeat process with next number
   add first number to second number
 */
 
